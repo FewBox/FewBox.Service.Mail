576d79a6833b803d0bc67d02e8f406b272675c4d
landpy <landpy@outlook.com>
2021-05-13 21:31:33 +0800

    Refine code.

---
+++ build.sh
@@ -0,0 +1,13 @@
+echo 'Build Mail...'
+DOCKER_REPO_SLUG=fewbox/mail PROJECTNAME=FewBox.Service.Mail PROJECTUNITTESTNAME=$PROJECTNAME.UnitTest
+dotnet restore $PROJECTNAME
+dotnet publish -c Release $PROJECTNAME/$PROJECTNAME.csproj -p:FileVersion=$TRAVIS_JOB_NUMBER
+cp Dockerfile ./$PROJECTNAME/bin/Release/netcoreapp5.0/publish/Dockerfile
+cp .dockerignore ./$PROJECTNAME/bin/Release/netcoreapp5.0/publish/.dockerignore
+cd $PROJECTNAME/bin/Release/netcoreapp5.0/publish
+docker build -t $DOCKER_REPO_SLUG:latest .
+docker push $DOCKER_REPO_SLUG:latest
+docker tag $DOCKER_REPO_SLUG:latest $DOCKER_REPO_SLUG:v1
+docker push $DOCKER_REPO_SLUG:v1
+cd ../../../../../
+echo 'Finished!'
