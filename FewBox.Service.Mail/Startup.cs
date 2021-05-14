578aaa6603062f9e1a32e890c2de0cee2575482f
landpy <landpy@outlook.com>
2021-05-14 14:22:08 +0800

    Refine code.

--- FewBox.Service.Mail/Startup.cs
+++ FewBox.Service.Mail/Startup.cs
@@ -18,6 +18,19 @@ namespace FewBox.Service.Mail
 {
     public class Startup
     {
+        private IList<ApiVersionDocument> ApiVersionDocuments = new List<ApiVersionDocument> {
+                new ApiVersionDocument{
+                    ApiVersion = new ApiVersion(1, 0),
+                    IsDefault = true
+                },
+                new ApiVersionDocument{
+                    ApiVersion = new ApiVersion(2, 0, "alpha1")
+                },
+                new ApiVersionDocument{
+                    ApiVersion = new ApiVersion(2, 0, "beta1")
+                }
+            };
+
         public Startup(IConfiguration configuration, IWebHostEnvironment hostingEnvironment)
         {
             this.Configuration = configuration;
@@ -31,7 +44,7 @@ namespace FewBox.Service.Mail
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddFewBoxSDK(FewBoxIntegrationType.MessageQueue, FewBoxListenerHostType.Web, FewBoxListenerType.Email);
-            services.AddFewBox(FewBoxDBType.None, FewBoxAuthType.Payload, new ApiVersion(1, 0, "alpha1"));
+            services.AddFewBox(this.ApiVersionDocuments, FewBoxDBType.None, FewBoxAuthType.Payload);
             var templateConfig = this.Configuration.GetSection("TemplateConfig").Get<TemplateConfig>();
             services.AddSingleton(templateConfig);
             var notificationTemplateConfig = this.Configuration.GetSection("NotificationTemplateConfig").Get<NotificationTemplateConfig>();
@@ -40,56 +53,12 @@ namespace FewBox.Service.Mail
             services.AddSingleton(smtpConfig);
             services.AddScoped<ISMTPService, SMTPService>();
             services.AddScoped<IMQMailHandler, MQMailHandler>();
-            // Used for Swagger Open Api Document.
-            services.AddOpenApiDocument(config =>
-            {
-                this.InitAspNetCoreOpenApiDocumentGeneratorSettings(config, "v1", new[] { "1-alpha1", "1-beta1", "1" }, "v1");
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseFewBox(new List<string> { "/swagger/v1/swagger.json" });
-        }
-
-        private void InitAspNetCoreOpenApiDocumentGeneratorSettings(AspNetCoreOpenApiDocumentGeneratorSettings config, string documentName, string[] apiGroupNames, string documentVersion)
-        {
-            config.DocumentName = documentName;
-            config.ApiGroupNames = apiGroupNames;
-            config.PostProcess = document =>
-            {
-                this.InitDocumentInfo(document, documentVersion);
-            };
-            config.OperationProcessors.Add(new OperationSecurityScopeProcessor("JWT"));
-            config.DocumentProcessors.Add(
-                new SecurityDefinitionAppender("JWT", new OpenApiSecurityScheme
-                {
-                    Type = OpenApiSecuritySchemeType.ApiKey,
-                    Name = "Authorization",
-                    Description = "Bearer [Token]",
-                    In = OpenApiSecurityApiKeyLocation.Header
-                })
-            );
-        }
-
-        private void InitDocumentInfo(OpenApiDocument document, string version)
-        {
-            document.Info.Version = version;
-            document.Info.Title = "FewBox Auth Api";
-            document.Info.Description = "FewBox Auth, for more information please visit the 'https://fewbox.com'";
-            document.Info.TermsOfService = "https://fewbox.com/terms";
-            document.Info.Contact = new OpenApiContact
-            {
-                Name = "FewBox",
-                Email = "support@fewbox.com",
-                Url = "https://fewbox.com/support"
-            };
-            document.Info.License = new OpenApiLicense
-            {
-                Name = "Use under license",
-                Url = "https://fewbox.com/license"
-            };
+            app.UseFewBox(this.ApiVersionDocuments);
         }
     }
 }
