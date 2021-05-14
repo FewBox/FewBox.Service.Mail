dcb27a5499c01c7b33df227070431b6330b7b4d5
landpy <landpy@outlook.com>
2021-05-14 13:49:21 +0800

    Refine code. [skip ci]

--- build-nas.sh
+++ build-nas.sh
@@ -4,14 +4,15 @@ DOCKER_REPO_VERSION=nas
 DOCKER_REPO_IP=192.168.1.38
 DOCKER_REPO_PORT=6088
 dotnet restore $PROJECTNAME
-dotnet publish -c Release $PROJECTNAME/$PROJECTNAME.csproj -p:FileVersion=$TRAVIS_JOB_NUMBER
-cp Dockerfile ./$PROJECTNAME/bin/Release/netcoreapp5.0/publish/Dockerfile
+dotnet publish -c Release $PROJECTNAME/$PROJECTNAME.csproj
+cp Nas/* ./$PROJECTNAME/bin/Release/netcoreapp5.0/publish
 cp .dockerignore ./$PROJECTNAME/bin/Release/netcoreapp5.0/publish/.dockerignore
 cd $PROJECTNAME/bin/Release/netcoreapp5.0/publish
 echo '*** docker folder ***'
 pwd
 echo '*** docker files ***'
-docker build -t $DOCKER_REPO_IP:$DOCKER_REPO_PORT/$DOCKER_REPO_SLUG:$DOCKER_REPO_VERSION .
+ls
+docker build --no-cache -t $DOCKER_REPO_IP:$DOCKER_REPO_PORT/$DOCKER_REPO_SLUG:$DOCKER_REPO_VERSION .
 docker push $DOCKER_REPO_IP:$DOCKER_REPO_PORT/$DOCKER_REPO_SLUG:$DOCKER_REPO_VERSION
 cd ../../../../../
 echo 'Finished!'
