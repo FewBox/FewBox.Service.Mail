ae1fbd8cf5afe4fc86f36f9f47cdb321100eb7e1
landpy <landpy@outlook.com>
2022-10-11 14:55:04 +0800

    Refine code.

--- build-nas.sh
+++ build-nas.sh
@@ -5,9 +5,9 @@ DOCKER_REPO_IP=192.168.1.38
 DOCKER_REPO_PORT=5000
 dotnet restore $PROJECTNAME
 dotnet publish -c Release $PROJECTNAME/$PROJECTNAME.csproj
-cp Nas/* ./$PROJECTNAME/bin/Release/netcoreapp5.0/publish
-cp .dockerignore ./$PROJECTNAME/bin/Release/netcoreapp5.0/publish/.dockerignore
-cd $PROJECTNAME/bin/Release/netcoreapp5.0/publish
+cp Nas/* ./$PROJECTNAME/bin/Release/netcoreapp6.0/publish
+cp .dockerignore ./$PROJECTNAME/bin/Release/netcoreapp6.0/publish/.dockerignore
+cd $PROJECTNAME/bin/Release/netcoreapp6.0/publish
 echo '*** docker folder ***'
 pwd
 echo '*** docker files ***'
